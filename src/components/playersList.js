d5173096c06121105511f21b98d7978bdca3bdbe
ahmed.magdy <ahmed.magdi@gmail.com>
2017-07-31 23:53:34 +0200

    score keep final

--- src/components/playersList.js
+++ src/components/playersList.js
@@ -6,16 +6,16 @@ export default class PlayersList extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            players: []
+            players: [],
+            sort:null
         }
     }
 
     componentDidMount() {
        axios.get('players.js')
-             .then(res => {
-               console.log(res.data)
-               this.setState({ players:res.data });
-             });
+         .then(res => {
+           this.setState({ players:res.data });
+        });
     }
     handleSubmit(e){
         e.preventDefault();
@@ -28,10 +28,20 @@ export default class PlayersList extends Component{
         })
         this.setState({players})
     }
+     toggleSort() {
+      const sortDesc = (a, b) => b.score - a.score;
+      const sortAsc = (a, b) => a.score - b.score;
+
+      this.setState({
+        players: this.state.players.sort(this.state.sort === 'desc' ? sortAsc : sortDesc),
+        sort: this.state.sort === 'desc' ? 'asc' : 'desc'
+      });
+    }
     render(){
 
         return(
             <div>
+            <button onClick={this.toggleSort.bind(this)}>sort</button>
                 {this.state.players.map((player)=>{
                     return <Player name={player.name} key={player._id} score={player.score}/>
                 })}
