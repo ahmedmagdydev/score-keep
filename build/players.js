f1d7b8db6944439108994b3c14e5e96244c38ed8
ahmed.magdy <ahmed.magdi@gmail.com>
2017-08-01 21:28:55 +0200

    build

---
+++ build/players.js
@@ -0,0 +1,38 @@
+[
+  {
+    "_id": "597f7f36f045059779481a18",
+    "index": 0,
+    "score": 10,
+    "name": "Nielsen Santiago"
+  },
+  {
+    "_id": "597f7f3647fc57b133c45cfa",
+    "index": 1,
+    "score": 41,
+    "name": "Caldwell Shaw"
+  },
+  {
+    "_id": "597f7f36f09d58d1b10d9114",
+    "index": 2,
+    "score": 9,
+    "name": "Langley Sims"
+  },
+  {
+    "_id": "597f7f3694ba7245189c9a04",
+    "index": 3,
+    "score": 16,
+    "name": "Espinoza Oconnor"
+  },
+  {
+    "_id": "597f7f365b4b31c83c206f5c",
+    "index": 4,
+    "score": 1,
+    "name": "Ballard Cline"
+  },
+  {
+    "_id": "597f7f3697f29e28bae73560",
+    "index": 5,
+    "score": 5,
+    "name": "Melinda Reid"
+  }
+]
